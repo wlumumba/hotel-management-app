82d477f297734d88b45f3f1f0269f2c60b291b95
Aaron Gingrich <gingrich.aaron1@gmail.com>
2021-09-30 14:16:17 -0500

    stuff

---
+++ src/sample/customerHomeController.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class customerHomeController {
+
+    @FXML
+    private Button customerLogoutButton;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void customerLogoutButtonClicked(ActionEvent event) throws IOException {
+        System.out.println("going back to loggin");
+        Stage stage;
+        Scene scene;
+        Parent root;
+
+        root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+    //Receive message from scene 1
+    public void transferData(String message) {
+        //Display the message
+        userName.setText(message);
+    }
+
+}
+
--- src/sample/loginController.java
+++ src/sample/loginController.java
@@ -2,8 +2,13 @@ package sample;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.*;
 
@@ -19,7 +24,7 @@ public class loginController {
     private Button adminButton;
 
     @FXML
-    private Button guestLog;
+    private Button guestButton;
 
     @FXML
     private Button customerButton;
@@ -36,7 +41,7 @@ public class loginController {
 
 
     @FXML
-    public void adminButtonClicked(Event e){
+    public void adminButtonClicked(Event e) throws IOException {
 
         System.out.println("admin login");
 
@@ -45,16 +50,68 @@ public class loginController {
         userNameLog.clear();
         passwordLog.clear();
 
-        if(validateUser(0, userName, password))
+        if(validateUser(1, userName, password)) {
             System.out.println("Access granted");
+            Stage stage;
+            Scene scene;
+            Parent root;
 
+            /////////////
+
+            //////////
+            root = FXMLLoader.load(getClass().getResource("adminHome.fxml"));
+            stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
         else
             System.out.println("Incorrect loggin info, please try again");
     }
 
-    @FXML
-    public void customerButtonClicked(Event e){
+    public void guestButtonClicked(Event e) throws IOException {
+
         System.out.println("customer login");
+        Stage stage;
+        Scene scene;
+        Parent root;
+
+        root = FXMLLoader.load(getClass().getResource("guestHome.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    @FXML
+    public void customerButtonClicked(Event e) throws IOException {
+        System.out.println("customer login");
+        //System.out.println("admin login");
+
+        String userName = userNameLog.getText();
+        String password = passwordLog.getText();
+        userNameLog.clear();
+        passwordLog.clear();
+
+        if(validateUser(0, userName, password)) {
+            System.out.println("Access granted");
+
+            Stage stage;
+            Scene scene;
+            Parent root;
+
+            root = FXMLLoader.load(getClass().getResource("customerHome.fxml"));
+            stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+
+        }
+        else
+            System.out.println("Incorrect loggin info, please try again");
     }
 
     @FXML
@@ -123,14 +180,16 @@ public class loginController {
 
         boolean exist = false;
         BufferedReader reader;
+        int accType;
         try {
             reader = new BufferedReader(new FileReader("C:\\Users\\Aaron Gingrich\\OneDrive - Triada\\Desktop\\hotelManagmentApp\\src\\sample\\user.txt"));
             String line = reader.readLine();
             while (line != null) {
                 // System.out.println(line);
                 String[] arrOfStr = line.split(",");
+                accType = Integer.parseInt(arrOfStr[0]);
 
-                if(arrOfStr[1].equals(userName) && arrOfStr[2].equals(password)) {
+                if(accType == accountType && arrOfStr[1].equals(userName) && arrOfStr[2].equals(password)) {
                     exist = true;
 
                 }
