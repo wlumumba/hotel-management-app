9247dc8c9163254403b2182be6bee874c6aa8e61
gingrich <gingrich.aaron1@gmail.com>
2021-10-07 15:32:07 -0500

    extra buttons

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/styles/login.fxml"));
-        primaryStage.setTitle("Hello World testy");
+        primaryStage.setTitle("Sleepless Nights B&B");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
