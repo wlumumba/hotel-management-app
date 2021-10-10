41f611f7788ba1a2ce918f0c4b3f482ecfed95e1
Blaze2121 <50607084+Blaze2121@users.noreply.github.com>
2021-10-10 15:34:38 -0500

    Added a better display output and for error input. If user does not add email then it requests it.

--- src/controller/adminHomeController.java
+++ src/controller/adminHomeController.java
@@ -2,6 +2,7 @@ package controller;
 
 import helpers.DBConnection;
 import helpers.User;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -10,15 +11,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.concurrent.TimeUnit;
 
 public class adminHomeController {
 
@@ -44,27 +48,35 @@ public class adminHomeController {
     @FXML
     private TextField createEmailAdmin;
     @FXML
-    private Text outputForButton;
+    private Label labelAdminOuput;
+    @FXML
+    private Label errorAdminOutput;
+
 
     @FXML
     public void createAdminButtonClicked(Event e) throws IOException {
         System.out.println("Create Admin Button");
 
         //checks for empty fields
-        if (createUserAdmin.getText().isEmpty()) {
+        if (createUserAdmin.getText().isEmpty()) { //errors here if one text box is empty
             System.out.println("Please enter Username");
+            errorAdminOutput.setText("Please enter a Username");
         }
         else if (createPassAdmin.getText().isEmpty()) {
             System.out.println("Please enter Password");
+            errorAdminOutput.setText("Please enter a Password");
         }
         else if (createFirstNAdmin.getText().isEmpty()) {
             System.out.println("Please enter First Name");
+            errorAdminOutput.setText("Please enter a First Name");
         }
         else if (createLastNAdmin.getText().isEmpty()) {
             System.out.println("Please enter Last Name");
+            errorAdminOutput.setText("Please enter a Last Name");
         }
         else if (createEmailAdmin.getText().isEmpty()) {
             System.out.println("Please enter Email");
+            errorAdminOutput.setText("Please enter an Email");
         }
         else {
             currentUser = new User(createUserAdmin.getText(), createFirstNAdmin.getText(), createLastNAdmin.getText(), createEmailAdmin.getText(), createPassAdmin.getText(), 1);
@@ -85,7 +97,7 @@ public class adminHomeController {
 
                 System.out.println("Success Inserted " + currentUser.toString());
 
-               // outputForButton.setText("Successful Admin Creation"); //maybe?
+                labelAdminOuput.setText("Success!"); //maybe?
 
                 //Clear ALL text fields
                 createUserAdmin.clear();
@@ -93,6 +105,13 @@ public class adminHomeController {
                 createFirstNAdmin.clear();
                 createLastNAdmin.clear();
                 createEmailAdmin.clear();
+                errorAdminOutput.setText("");
+                //****************DISPLAYS TO THE USER SUCCESS THEN CLEARS DISPLAY TEXT********//
+                PauseTransition pause = new PauseTransition();
+                pause.setDuration(Duration.seconds(2));
+                pause.setOnFinished(event->labelAdminOuput.setText(" "));
+                pause.play();
+
             }
             catch (SQLIntegrityConstraintViolationException ex) {
                 System.out.println("Username taken! Try another one");
